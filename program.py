ecea1d23a6dbf66265dff98d208148fedb85062f
yangbitao001 <yangbitao001@ke.com>
2021-01-12 18:23:15 +0800

    添加SAR模型

--- program.py
+++ program.py
@@ -118,7 +118,7 @@ def build_loss(flags):
 
 def build_trainer(model, optimizer, loss, train_loader, val_loader, \
                   device, flags, global_state):
-    if flags.Global.algorithm in ['CRNN', 'FAN', 'GRCNN', 'DAN']:
+    if flags.Global.algorithm in ['CRNN', 'FAN', 'GRCNN', 'DAN', 'SAR']:
         trainer = TrainerRec(
             device=device,
             model=model,
